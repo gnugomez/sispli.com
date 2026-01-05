c95f64fe575895ba453c193af07e74e032ca497a
Jordi Gómez <jordi.gomez@eclipse-foundation.org>
2026-01-06 00:29:14 +0100

    fix: preventing prose max-w limitaion

--- app/components/content/posts-stack.vue
+++ app/components/content/posts-stack.vue
@@ -6,7 +6,7 @@ const { data: posts } = await useAsyncData("posts-stack", () => queryCollection(
   <div class="posts-stack">
     <div class="post" v-for="post in posts" :key="post.id">
       <h2>{{ post.title }}</h2>
-      <div class="prose">
+      <div class="prose max-w-full">
         <ContentRenderer :value="post.meta.body as Record<string, unknown>" />
       </div>
     </div>
