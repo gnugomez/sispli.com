7f0a7f55575a23ca93b11e24c4226438cc650bff
Jordi Gómez <jordi.gomez@eclipse-foundation.org>
2026-02-02 23:27:59 +0100

    feat: updating blog's layout

--- app/components/content/posts-stack.vue
+++ app/components/content/posts-stack.vue
@@ -1,13 +1,16 @@
 <script lang="ts" setup>
-const { data: posts } = await useAsyncData("posts-stack", () => queryCollection("posts").where("draft", "=", false).order("createdAt", "DESC").all());
+const { data: posts } = await useAsyncData('posts-stack', () => queryCollection('posts').where('draft', '=', false).order('createdAt', 'DESC').all())
+
+type Content = Record<string, unknown>
 </script>
 
 <template>
   <div class="posts-stack">
-    <div class="post" v-for="post in posts" :key="post.id">
-      <h2>{{ post.title }}</h2>
+    <div v-for="post in posts" :key="post.id" class="post">
       <div class="prose max-w-full">
-        <ContentRenderer :value="post.meta.body as Record<string, unknown>" />
+        <h2>{{ post.title }}</h2>
+        <span v-if="post.createdAt">{{ new Date(post.createdAt).toLocaleDateString() }}</span>
+        <ContentRenderer :value="post.meta.body as Content" />
       </div>
     </div>
   </div>
